5e6f10eef9111dee44cda06c94db692f5c25a62b
Charles Eubanks <charles.patrick.eubanks@gmail.com>
2020-08-01 15:45:59 -0400

    add oauth routing,fix require Router in index router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get("/", function(req, res) {
     res.render("index");
 });
 
-module.exports = router
+// login route
+router.get('/authorize', passport.authenticate('spotify', { scope: ['user-read-private', 'user-read-email'] }));
+
+// google callback route
+router.get('/oauth2callback', passport.authenticate('spotify', {
+    successRedirect: '/',
+    failureRedirect: '/'
+}));
+
+// logout route
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
