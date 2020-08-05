d90efe8b4f9914f783778223f6a3493a6f2515b4
Charles Eubanks <charles.patrick.eubanks@gmail.com>
2020-08-05 19:50:37 -0400

    add update and delete functions

--- controllers/albums.js
+++ controllers/albums.js
@@ -4,7 +4,11 @@ const Track = require("../models/track");
 
 module.exports = {
     index,
-    create
+    create,
+    show,
+    delete: deleteAlbum,
+    edit,
+    update
 };
 
 function index(req, res) {
@@ -24,5 +28,38 @@ function create(req, res) {
             res.redirect("/albums");
 
         });
+    });
+}
+
+function show(req, res) {
+    Album.findById(req.params.id)
+        .populate("tracks").exec(function(err, album) {
+            Track.find({ _id: { $nin: album.tracks } },
+                function(err, tracks) {
+                    res.render("albums/show", { title: "Album Details", user: req.user, album, tracks });
+
+                });
+        });
+}
+
+function deleteAlbum(req, res) {
+    Album.findByIdAndDelete(req.params.id, function(err, album) {
+        res.redirect("/albums", { user: req.user });
+    });
+}
+
+function edit(req, res) {
+    Album.findById(req.params.id)
+        .populate("tracks").exec(function(err, album) {
+            Track.find({ _id: { $nin: album.tracks } },
+                function(err, tracks) {
+                    res.render("albums/edit", { title: "Edit Album", user: req.user, album, tracks });
+                });
+        });
+}
+
+function update(req, res) {
+    Album.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(err, album) {
+        res.redirect(`/albums/${album._id}`, { user: req.user });
     });
 }
